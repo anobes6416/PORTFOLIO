e8b966664071ffe4b732b4b6ec2372d6397b8096
Sebona Ifa <sebona@sebona.localdomain>
2024-05-15 14:42:35 +0300

    portfolio

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='p-5 bg-primary flex justify-between'>
+      <h className="text-secondary text-4xl font-semibold">S</h>
+      <h className="text-white text-4xl font-semibold">I</h>
+      <h className="text-tertiary text-4xl font-semibold">N</h>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/Home/Intro.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div className='h-[80vh] bg-primary flex flex-col items-start justify-center gap-8'>
+        <h1 className="text-white font-semibold">Hi , I am</h1>
+        <h1 className="text-7xl text-secondary font-semibold">Sebona Ifa</h1>
+        <h1 className="text-7xl text-white font-semibold">I am FullStack Website Developer</h1>
+        <p className="text-white w-2/3">
+        Hello! My name is Sebona Ifa, and I am thrilled to present my portfolio
+        as a software engineering student and experienced full-stack developer. 
+        I have spent the past four years honing my skills and expanding my knowledge
+        in various programming languages, frameworks, and technologies.
+        </p>
+        <button className="border-2 border-tertiary px-10 py-3 text-tertiary rounded">Get Started</button>
+    </div>
+  )
+}
+
+export default Intro
